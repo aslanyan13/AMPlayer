4046fd4e8ebbf9a3aeea7a3e20cc2d65a5a095b3
aslanyan13 <hro.2016@yandex.ru>
2021-03-10 00:24:53 +0400

    Small fixes

--- main.cpp
+++ main.cpp
@@ -10,15 +10,26 @@ int main(int argc, char *argv[])
 {
     srand(time(NULL));
 
+    QApplication a(argc, argv);
+
     if (!BASS_Init(-1, 44100, BASS_DEVICE_16BITS | BASS_DEVICE_STEREO, 0, NULL)) {
         QMessageBox msgBox;
+        msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
         msgBox.setText("No audio devices found!");
-        return msgBox.exec();
+        msgBox.setStyleSheet("background-color: #141414; color: silver;");
+        msgBox.setInformativeText("Continue without audio?");
+        int ret = msgBox.exec();
+
+        switch (ret) {
+          case QMessageBox::No:
+              exit(0);
+              break;
+          default:
+              break;
+        }
     }
 
     BASS_SetConfig(BASS_CONFIG_SRC, 16);
-
-    QApplication a(argc, argv);
 
     MainWindow w;
     w.show();
